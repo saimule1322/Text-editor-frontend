8692a496fa22825118adc5959dcea0e7acd8d71d
Mule Venkata Sai Prasanna Kumar Reddy <saimule1322@gmail.com>
2025-03-07 10:01:53 +0530

    Update Customfetch.ts

--- src/Utils/Customfetch.ts
+++ src/Utils/Customfetch.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
+
+// Determine the base URL for the API based on the environment
+const baseURL = import.meta.env.VITE_BACKEND_URL || '/api'; // Fallback to '/api' for local dev (proxy will handle this)
+
 const customFetch = axios.create({
-    baseURL: '/api',
-  });
-  
+  baseURL, // Use the dynamic baseURL
+});
+
 export default customFetch;
 
 
